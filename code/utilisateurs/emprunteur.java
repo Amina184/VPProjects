44bf222a89a003a9f4259e8660e5d574e876aef3
Amina184 <aminaambari2003@gmail.com>
2022-04-17 17:17:08 +0000

    Create emprunteur.java

---
+++ code/utilisateurs/emprunteur.java
@@ -0,0 +1,21 @@
+package utilisateurs;
+
+import java.util.*;
+
+public class emprunteur {
+
+	
+	private int idEmp;
+	private String nom;
+	private String prenom;
+	private String cin;
+
+	/**
+	 * 
+	 * @param L
+	 */
+	public boolean chercher(titre L) {
+		return get
+	}
+
+}
