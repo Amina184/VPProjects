e04523010162d10cb1fc2486f030aef89d1ac6cb
Amina184 <aminaambari2003@gmail.com>
2022-04-17 17:16:04 +0000

    Create TestEmprunteur.java

---
+++ code/testBib/TestEmprunteur.java
@@ -0,0 +1,20 @@
+package testBib;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestEmprunteur {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	
+}
