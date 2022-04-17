cfe3bb2db0e1ea95acd08158a14a1caf4a0d6feb
Amina184 <aminaambari2003@gmail.com>
2022-04-17 17:16:07 +0000

    Create bibliothécaire.java

---
+++ code/utilisateurs/bibliothécaire.java
@@ -0,0 +1,18 @@
+package utilisateurs;
+
+import java.util.*;
+
+public class bibliothécaire {
+
+	//Collection<emprunt> emprunt;
+	private int idBib;
+	private String prenom;
+	private String nom;
+
+	public ArrayList lister() {
+	  
+		ArrayList T = new ArrayList();
+		return T;
+	}
+
+}
