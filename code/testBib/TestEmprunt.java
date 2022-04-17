9c679dc84a4f0b29738e9521010e52dd2e8b152c
Amina184 <aminaambari2003@gmail.com>
2022-04-17 17:16:48 +0000

    Create TestEmprunt.java

---
+++ code/testBib/TestEmprunt.java
@@ -0,0 +1,29 @@
+package testBib;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestEmprunt {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testGetDateEmprunt() {
+		System.out.print("test passe");
+	}
+
+	@Test
+	void testSetDateRetour() {
+		System.out.print("test passe"); 
+	}
+
+}
