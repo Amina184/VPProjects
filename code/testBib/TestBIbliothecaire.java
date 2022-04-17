2c58e84eaf4031ca259e81764cbaeebfdedcef85
Amina184 <aminaambari2003@gmail.com>
2022-04-17 17:15:55 +0000

    Create TestBIbliothecaire.java

---
+++ code/testBib/TestBIbliothecaire.java
@@ -0,0 +1,26 @@
+package testBib;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestBIbliothecaire {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testLister() {
+		System.out.print("test passe");
+	}
+
+	
+
+}
